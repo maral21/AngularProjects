42ea87a558368bd130f89c78982919cd0eb83afe
Mar Andrés <mar.andres.l@gmv.com>
2020-11-23 16:01:01 +0100

    "Communicating with angular child components"

--- ng-fundamentals/src/app/events/events-list.component.ts
+++ ng-fundamentals/src/app/events/events-list.component.ts
@@ -5,23 +5,13 @@ import {Component} from '@angular/core'
     template: `
     <div>
         <h1>Upcoming Angular Events</h1>
-        <hr>
-        <div class="well hoverwell thumbnail">
-            <h2>{{event.name}}</h2>
-            <div>Date: {{event.date}}</div>
-            <div>Time: {{event.time}}</div>
-            <div>Price: \${{event.price}}</div>
-            <div>
-                <span>Location: {{event.location.address}}</span>
-                <span>&nbsp;</span>
-                <span>{{event.location.city}}, {{event.location.country}}</span>
-            </div>
-        </div>
+        <hr/>
+        <event-thumbnail [event]="event1"></event-thumbnail>
     </div>
-    `
+    ` 
 })
 export class EventsListComponent{
-    event = {
+    event1 = {
         id: 1,
         name: 'Angular Connect',
         date: '09/26/2036',
